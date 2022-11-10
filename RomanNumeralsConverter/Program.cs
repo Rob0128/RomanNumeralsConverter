9a44debd69195c06be111c9c897917c54486644a
Robert Hill <RobertHill@paypoint.com>
2022-11-10 22:19:02 +0000

    Add project files.

---
+++ RomanNumeralsConverter/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RomanNumeralsConverter.Functions;
+
+
+ConverterService converter = new ConverterService();
+
+while (true)
+{
+    Console.WriteLine("Enter number up to 1000:");
+
+    //read input
+    string line = Console.ReadLine();
+    int number;
+
+    //check input is a valid number between 1 and 1000
+    if (!int.TryParse(line, out number))
+        {
+            Console.WriteLine("Please enter a valid number");
+        }
+    else if (number <= 0 || number > 1000)
+        {
+            Console.WriteLine("Please enter a number between 0 and 1000");
+        }
+    //if number is valid it is passed to the converter and the result is printed
+    else
+        {
+            string romanNumeral = converter.ConvertNumberToRoman(number);
+
+            Console.WriteLine("result: " + romanNumeral);
+        }
+  
+}
+   
---
+++ RomanNumeralsConverter/Services/ConverterService.cs
@@ -0,0 +1,27 @@
+﻿
+namespace RomanNumeralsConverter.Functions
+{
+    internal class ConverterService
+    {
+        public string ConvertNumberToRoman(int number)
+        {
+            int givenNum = number;
+            List<string> resultRomanNumerals = new List<string>();
+            List<int> numberValues = new List<int>() { 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1  };
+            List<string> romanNumerals = new List<string>() { "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+
+            //two lists of numbers and their roman numeral conterparts are iterated through until the input 'number' is greater than the current 'numberValue'
+            for (int x = 0; x < numberValues.Count; x++)
+            {
+                while(givenNum >= numberValues.ElementAt(x))
+                {
+                    //the 'numberValue' is subtracted from the input 'number' and the numberValue's romanNumeral equivilent is added to the 'resultRomanNumerals' list
+                    givenNum = givenNum - numberValues.ElementAt(x);
+                    resultRomanNumerals.Add(romanNumerals.ElementAt(x));
+                }
+            }
+            //the 'resultRomanNumerals' List is converted to a string and returned
+            return String.Join("", resultRomanNumerals);
+        }
+    }
+}
